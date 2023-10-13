5329ae940834ece4ef950127a7da6c23ff0cbb40
szt2vetmat <szt2vetmat@vasvari.org>
2023-10-13 10:19:52 +0200

    2.Feladat kesz

--- object.js
+++ object.js
@@ -1,6 +1,23 @@
+//1.Feladat
+
+
 var user = {};
 user.firstName = "John";
 user.lastName = "Smith";
 user.firstName = "Paul";
 delete user.firstName;
 console.log(user);
+
+//2.Feladat
+
+function isEmpty(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(isEmpty({}));                  
+  console.log(isEmpty({name: "John"}));
